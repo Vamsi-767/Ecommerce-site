c8254e6eb2467f53ccfe996fe9011d2187db9fe9
Vamsi-767 <vamsikrishnabodaballa7@gmail.com>
2025-03-14 16:56:21 -0500

    updated price for samsung

--- src/pages/ProductListing.js
+++ src/pages/ProductListing.js
@@ -8,7 +8,7 @@ const products = [
   { id: 1, name: 'HP Pavilion', price: '$649', description: 'HP 15-inch with 16GB RAM 1TB SSD powered by Intel i7', image: 'image/hp1.jpg', category: 'Laptop' },
   { id: 2, name: 'Lenovo YOGA', price: '$450', description: 'Lenovo 15-inch with 8GB RAM 512GB SSD powered by Intel i7', image: 'image/lenovo.jpg', category: 'Laptop' },
   { id: 3, name: 'MacBook Air', price: '$1199', description: 'MacBook Air 13-inch with M2 chip', image: 'image/mac.jpg', category: 'Laptop' },
-  { id: 4, name: 'Samsung Galaxy Tab', price: '$799', description: 'Samsung Galaxy Tab with 128GB storage', image: 'image/tab.jpg', category: 'Mobile' },
+  { id: 4, name: 'Samsung Galaxy Tab', price: '$599', description: 'Samsung Galaxy Tab with 128GB storage', image: 'image/tab.jpg', category: 'Mobile' },
   { id: 5, name: 'iPhone 12', price: '$699', description: 'Apple iPhone 12 with 64GB storage', image: 'image/iphone.jpg', category: 'Mobile' },
   { id: 6, name: 'OnePlus 9', price: '$729', description: 'OnePlus 9 with 8GB RAM and 128GB storage', image: 'image/oneplus.jpg', category: 'Mobile' }
 ];
